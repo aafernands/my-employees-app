6a79b53624f076dc39ba95ffbe1b1e34c27cc3e9
Alex Fernandes <alexfernands@outlook.com>
2021-01-21 18:09:53 -0500

    search result

--- src/App.js
+++ src/App.js
@@ -4,26 +4,39 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import EmployeeTable from "./components/EmployeeTable";
 import SearchComponent from "./components/SearchComponent";
 import { useEffect, useState } from "react";
-import { format } from 'date-fns';
+import { format } from "date-fns";
 
 function App() {
+	const [originalList, setOriginalList] = useState([]);
 	const [employeeList, setEmployeeList] = useState([]);
+
+	const handleSearchChange = (event) => {
+    const filterText = event.target.value;
+    const filterUser = originalList.filter(user => {
+      return user.name.toLowerCase().indexOf(filterText) > -1;
+    })
+
+    setEmployeeList(filterUser);
+	};
+
 	useEffect(() => {
 		fetch("https://randomuser.me/api?results=15")
 			.then((response) => response.json())
 			.then((response) => {
 				console.log(response);
-				setEmployeeList(
-					response.results.map((user) => {
-						return {
-							name: user.name.first,
-							dob: format(new Date(user.dob.date), 'MM-dd-yyyy'),
-							email: user.email,
-							picture: user.picture.thumbnail,
-							phone: user.phone,
-						};
-					})
-				);
+
+				const userMap = response.results.map((user) => {
+					return {
+						name: user.name.first,
+						dob: format(new Date(user.dob.date), "MM-dd-yyyy"),
+						email: user.email,
+						picture: user.picture.thumbnail,
+						phone: user.phone,
+					};
+				});
+
+				setEmployeeList(userMap);
+				setOriginalList(userMap);
 			});
 	}, []);
 
@@ -42,7 +55,9 @@ function App() {
 				</p>
 			</div>
 
-			<SearchComponent></SearchComponent>
+			<SearchComponent
+				handleSearchChange={handleSearchChange}
+			></SearchComponent>
 			<EmployeeTable employees={employeeList}></EmployeeTable>
 		</div>
 	);
--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
-function SearchComponent() {
+function SearchComponent({ handleSearchChange }) {
 	return (
 		<form>
 			<div className="row g-3 align-items-center">
 				<div className="col-auto">
-					<input type="text" id="searchInput" className="form-control" />
+					<input
+						type="text"
+						id="searchInput"
+						placeholder="Search"
+						onChange={handleSearchChange}
+						className="form-control"
+					/>
 				</div>
 				<button className="btn btn-primary">Search</button>
 			</div>
